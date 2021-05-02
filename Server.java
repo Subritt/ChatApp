1aacfbba9dd00182ef87cc4551dd409d7002e495
Subritt <subrittburlakoti@gmail.com>
2021-05-02 23:52:19 +0545

    defined ServerSocket, Socket, BufferReader, PrintWriter and Reader function

--- Server.java
+++ Server.java
@@ -1,18 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 class Server {
 
     ServerSocket server;
-    Socket socker;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
 
     // Constructor
     public Server() {
-        System.out.println("server constructor...");
+        try {
+            server = new ServerSocket(8888);
+            System.out.println("Server is ready!");
+            System.out.println("waiting for connection...");
+            socket = server.accept();
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void startReading() {
+        Runnable r1 = () -> {
+            System.out.println("reader started...");
+
+            while(true) {
+                String msg;
+                try {
+                    msg = br.readLine();
+                    if(msg.equals("exit")) {
+                        System.out.println("Client terminated the chat");
+                        break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
-    public static void main(String[] args) {
+    public void startWriting() {
+	}
+
+
+	public static void main(String[] args) {
         System.out.println("this is server...");
         new Server();
     }
 }
+
