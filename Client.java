363f18b1473d132339bcba8a7ef3c77fd6f108ab
Subritt <subrittburlakoti@gmail.com>
2021-05-03 17:58:31 +0545

    Client GUI completed

--- Client.java
+++ Client.java
@@ -1,34 +1,124 @@
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+
+public class Client extends JFrame{
     
     Socket socket;
     BufferedReader br;
     PrintWriter out;
+
+    // Components
+    private JLabel heading = new JLabel("Client");
+    private JTextArea displayArea = new JTextArea();
+    private JTextField msgInput = new JTextField();
+    private Font font = new Font("Roboto", Font.PLAIN, 20);
 
     // Constructor
     public Client() {
         try {
 
             System.out.println("sending request to server...");
-            socket = new Socket("127.0.0.1",8888);
+            socket = new Socket("127.0.0.1",8000);
             System.out.println("connection established!");
 
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream());
 
+            createGUI();
+            handleEvents();
+
             startReading();
-            startWriting();
+            // startWriting();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Handle Events
+    private void handleEvents() {
+        msgInput.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // System.out.println(e.getKeyCode() + " Released!");
+
+                if (e.getKeyCode() == 10) {
+                    // System.out.println("you've pressed enter button");
+                    String contentToSend = msgInput.getText();
+                    // System.out.println(contentToSend);
+                    displayArea.append("Me : " + contentToSend + "\n");
+                    out.println(contentToSend);
+                    out.flush();
+                    msgInput.setText("");
+                    msgInput.requestFocus();
+                }
+            }
+
+        });
+    }
+
+    // Create GUI
+    private void createGUI() {
+        this.setTitle("Client Messanger");
+        this.setSize(600, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // For components
+        heading.setFont(font);
+        displayArea.setFont(font);
+        msgInput.setFont(font);
+        // heading.setIcon(new ImageIcon("client.jpg"));
+        heading.setHorizontalTextPosition(SwingConstants.CENTER);
+        heading.setVerticalTextPosition(SwingConstants.BOTTOM);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        
+        displayArea.setEditable(false);
+        
+        // Layout
+        this.setLayout(new BorderLayout());
+        
+        // Adding components to the frame
+        this.add(heading,BorderLayout.NORTH);
+        JScrollPane jScrollPane = new JScrollPane(displayArea);
+        this.add(jScrollPane, BorderLayout.CENTER);
+        // this.add(displayArea, BorderLayout.CENTER);
+        this.add(msgInput, BorderLayout.SOUTH);
+        
+        this.setVisible(true);
+    }
+    
     // Reader
     public void startReading() {
         Runnable r1 = () -> {
@@ -40,10 +130,13 @@ public class Client {
                     msg = br.readLine();
                     if(msg.equals("exit")) {
                         System.out.println("Server terminated the chat");
+                        JOptionPane.showMessageDialog(this, "Server terminated the chat");
+                        msgInput.setEnabled(false);
                         socket.close();
                         break;
                     }
-                    System.out.println("Server : " + msg);
+                    // System.out.println("Server : " + msg);
+                    displayArea.append("Server : " + msg + "\n");
                 } catch (IOException e) {
                     // e.printStackTrace();
                     // System.out.println("e: Server terminated the chat");
